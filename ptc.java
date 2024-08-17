17a060452d8ee1847e4d0d8d24bc0c3bdde4e0db
shoaib <khanshoaib6306@gmail.com>
2024-08-18 00:31:48 +0530

    code

--- ptc.java
+++ ptc.java
@@ -1,15 +1,17 @@
-interface myclass{
-	 void show();
-}
-class demo implements myclass{
-	 void show(){
-		System.out.println("this is interface");
-		}
+class parents{
+	void demo(){
+		System.out.println("parents");
 	}
-//}
+}
+class child extends parents{
+	void demo2(){
+		System.out.println("child");
+	}
+}
 class main{
 	public static void main(String args[]){
-	demo m=new demo();
-	m.show();
+		child c=new child();
+		c.demo();
+		c.demo2();
 	}
 }
